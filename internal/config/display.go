545cb97266b2fa66005fd8caae0aa63147d9f391
MustBreathe <lukasiewicz86@gmail.com>
2026-07-29 10:49:17 +0200

    apply naming convention

--- internal/config/display.go
+++ internal/config/display.go
@@ -7,16 +7,16 @@ import (
 	"strings"
 )
 
-type DisplayConfiguration struct {
+type Display struct {
 	// Display configuration for the watchdog application
 	// This struct can be extended to include more display-related settings as needed
 }
 
-func NewDisplayConfiguration() (*DisplayConfiguration, error) {
-	return &DisplayConfiguration{}, nil
+func NewDisplayConfiguration() (*Display, error) {
+	return &Display{}, nil
 }
 
-func (dc *DisplayConfiguration) Run() error {
+func (dc *Display) Run() error {
 	log.Println("Current configuration loaded")
 	conf := os.Environ()
 	slices.Sort(conf)
@@ -27,10 +27,5 @@ func (dc *DisplayConfiguration) Run() error {
 		}
 	}
 
-	for _, e := range conf {
-		if !strings.HasPrefix(e, "WATCHDOG_") {
-			log.Println(e)
-		}
-	}
 	return nil
 }
