fd35eeaeef167beff2669a7f82d169f2a1d5e51a
Michal Lukasiewicz <lukasiewicz.michal@outlook.com>
2026-08-09 23:48:52 +0200

    changes after hermes review

--- internal/monitor/MonitorService.go
+++ internal/monitor/MonitorService.go
@@ -2,8 +2,6 @@ package monitor
 
 import "database/sql"
 
-ty
-
 type MonitorService struct {
 	db *sql.DB
 }
@@ -14,7 +12,6 @@ func CreateMonitorService(db *sql.DB) *MonitorService {
 	}
 }
 
-
 func (s MonitorService) GetMonitors() ([]MonitorDTO, error) {
 	rows, err := s.db.Query("SELECT id, name, url FROM monitors")
 
@@ -23,17 +20,15 @@ func (s MonitorService) GetMonitors() ([]MonitorDTO, error) {
 	}
 	defer rows.Close()
 
-	monitors := make([]MonitorDTO, rows.length)
+	monitors := make([]MonitorDTO, 0)
 
-
-	for i:=0; rows.Next(); i++ {
+	for i := 0; rows.Next(); i++ {
 		var monitor MonitorDTO
 		if err := rows.Scan(&monitor.ID, &monitor.Name, &monitor.URL); err != nil {
 			return nil, err
 		}
-		monitors := append(monitors, monitor)
+		monitors = append(monitors, monitor)
 	}
 
-
-	return err
+	return monitors, err
 }
